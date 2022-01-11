ec26fa034abba4c178a5105932e0f031260eade4
franklin selva <franklinselva10@gmail.com>
2022-01-11 14:30:19 +0100

    Automated initial setup scripts

---
+++ first_setup.sh
@@ -0,0 +1,12 @@
+# Copyright 2022 Franklin Selva. All rights reserved.
+# Use of this source code is governed by a BSD-style
+# license that can be found in the LICENSE file.
+
+echo "INSTALLING UPF"
+rm -rf upf && git clone https://github.com/aiplan4eu/upf && pip install upf/
+
+echo "GENERATING PYTHON PROTO PARSER"
+python -m grpc_tools.protoc -I=./python/ --python_out=./python --grpc_python_out=./python/  ./python/upf.proto
+
+echo "GENERATING RUST PROTO PARSER"
+cargo build
